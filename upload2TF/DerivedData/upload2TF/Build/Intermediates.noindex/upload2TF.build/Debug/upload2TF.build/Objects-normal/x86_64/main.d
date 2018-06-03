58cbaedd3b6b8bfdba663a734dca9007b8e364a2
易礼明 <klarkyi@klark-MacBook-Pro.local>
2018-06-03 15:58:33 +0800

    del DerivedData file

--- upload2TF/DerivedData/upload2TF/Build/Intermediates.noindex/upload2TF.build/Debug/upload2TF.build/Objects-normal/x86_64/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: /Users/klarkyi/Desktop/Upload2TF/upload2TF/upload2TF/main.m \
-  /Users/klarkyi/Desktop/Upload2TF/upload2TF/upload2TF/nstask.h
--- upload2TF/DerivedData/upload2TF/Build/Intermediates.noindex/upload2TF.build/Debug/upload2TF.build/Objects-normal/x86_64/nstask.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/klarkyi/Desktop/Upload2TF/upload2TF/upload2TF/nstask.m \
-  /Users/klarkyi/Desktop/Upload2TF/upload2TF/upload2TF/nstask.h
